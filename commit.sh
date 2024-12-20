59723cc101418ee76fb36c50ff0fe44e2ddf5b43
Sutdown <2155197641@qq.com>
2024-12-20 18:38:12 +0800

    coroutine

--- commit.sh
+++ commit.sh
@@ -1,3 +1,3 @@
 git add .
-git commit -m " LRU cache "
+git commit -m "coroutine"
 git push origin main
---
+++ 协程/timer.cc
@@ -0,0 +1,41 @@
+#if !CO_CONTEXT_NO_GENERATOR
+
+#include <co_context/generator.hpp>
+
+#include <iostream>
+#include <ranges>
+
+co_context::generator<int> iota(int x)
+{
+  while (true)
+  {
+    co_yield x;
+    ++x;
+  }
+}
+
+int main()
+{
+  using std::views::drop, std::views::take;
+
+  for (auto &&x : iota(1) | drop(5) | take(3))
+  {
+    std::cout << x << " ";
+  }
+
+  return 0;
+}
+
+#else // if !CO_CONTEXT_NO_GENERATOR
+
+#include <iostream>
+
+int main()
+{
+  std::cout
+      << "This program requires g++ 11.3 or clang 17 as the compiler. exit..."
+      << std::endl;
+  return 0;
+}
+
+#endif
